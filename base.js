215ed7af8d943928ab8a8488546c29ed1f9ea244
kenny fung <kenny.fung@loveholidays.com>
2021-04-15 13:30:35 +0100

    add more base rules

--- base.js
+++ base.js
@@ -10,5 +10,33 @@ module.exports = {
     '@typescript-eslint/no-shadow': 'off',
     'no-return-assign': ['error', 'except-parens'],
     'no-plusplus': ['error', { 'allowForLoopAfterthoughts': true }],
-  }
+
+    // -- No default exports
+    'import/no-default-export': 'error',
+    'import/prefer-default-export': 'off',
+
+    // -- Code formatting
+    '@typescript-eslint/member-delimiter-style': 'error',
+    'array-bracket-spacing': ['error', 'always'],
+    'comma-dangle': ['error', 'always-multiline'],
+    '@typescript-eslint/comma-dangle': ['error', 'always-multiline'],
+    'max-len': ['error', { code: 100, ignoreComments: true, ignoreUrls: true, ignoreStrings: true, }],
+    'no-multiple-empty-lines': ['error', {
+      max: 1,
+      maxEOF: 1,
+      maxBOF: 0,
+    }],
+  },
+
+  overrides: [
+    {
+      rules: {
+        'max-len': 'off',
+      },
+      files: [
+        'src/**/*.spec.ts',
+        'src/**/*.spec.tsx',
+      ],
+    },
+  ]
 }
